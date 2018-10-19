7c130c18addcc2abe787ba673891c076607694f3
liquan666 <43060923+liquan666@users.noreply.github.com>
2018-10-19 09:37:06 +0800

    Update 3.6.py

--- 3.6.py
+++ 3.6.py
@@ -1,9 +1,6 @@
 import time
 scale = 50
-t = time.clock()
 for i in range(scale + 1):
     a = '.' * i
-    c = (i / scale) * 100
-    t -= time.clock()
-    print("\rStarting {:^3.0f}%[{}]{:.2f}s Done".format(c,a,-t), end='')
+    print("\rStarting{}Done".format(a), end='')
     time.sleep(0.05)
